e72bb73135fb355c793680b79f6be6d0e52ede4b
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-01-27 05:38:39 +0530

    added mail many function

--- MailerGui/Screens/utils/mailto.py
+++ MailerGui/Screens/utils/mailto.py
@@ -6,12 +6,41 @@ import os
 
 import base64
 import sendgrid
+from celery import Celery
 from dotenv import load_dotenv
 from sendgrid.helpers.mail import *
 
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
+
+def emailAll(sender,tos,message,subject,attach=False):
+    sg = sendgrid.SendGridAPIClient(apikey=API_KEY)
+    from_email = Email(sender)
+    content = Content("text/html",message)
+    if(bool(attach)):
+        with open(attach,'rb') as f:
+            data = f.read()
+            f.close()
+        encoded = base64.b64encode(data).decode()
+        attachment = Attachment()
+        attachment.content = encoded
+        attachment.type = ''  ## application/pdf for pdf attach and image/(.jpg,.jpeg,.png)
+        attachment.filename = '' ## file name 
+        attachment.disposition = 'attachment' # either inline or attachment
+        attachment.content_id = 'DSC' ## any valid id
+        for to in tos:
+            try:
+                to_email = Email(to)
+                mail = Mail(from_email, subject, to_email, content)
+                mail.add_attachment(attachment)
+                response = sg.client.mail.send.post(request_body=mail.get())
+                print("mail sent to",to)
+                print(attach)
+                print(response.status_code)
+            except:
+                print("mail not sent to ", to)
+                pass
 
 def email(sender,to,message,subject,attach=False):
     sg = sendgrid.SendGridAPIClient(apikey=API_KEY)
